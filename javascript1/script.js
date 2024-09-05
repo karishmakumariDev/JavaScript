040621737f5cb63e6a17b9df22fa56a08c8874a7
karishmakumariDev <kumarikarishma311329>
2024-09-05 16:01:24 +0530

    getFactorial fun added

--- javascript1/script.js
+++ javascript1/script.js
@@ -5,3 +5,15 @@ const addNumber = (num1, num2) => {
 }
 
 addNumber(2, 3);
+
+
+
+function getFactorial(num) {
+    let ans = 1;
+    for(let i = 1; i <= num; i++) {
+        ans *= i;
+    }
+    return ans;
+}
+
+console.log(getFactorial(5));
