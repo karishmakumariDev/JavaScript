35509ce5fb941f88cb29707ce3fd0b3fa5e96d99
karishmakumariDev <kumarikarishma311329>
2024-09-19 15:48:27 +0530

    add searchbar

--- js-project3/script.js
+++ js-project3/script.js
@@ -1,17 +1,68 @@
+
+const nameInput = document.getElementById('nameInput');
+const button = document.getElementById('red-button');
+const userDetail = document.getElementById('userDetail');
+const message = document.getElementById('message'); 
+
 let users = [
-    { name: "Manish", age: 25, email: "john@example.com", password: "john123" },
-    { name: "Bikash", age: 30, email: "jane@example.com", password: "jane456" },
-    { name: "Karishma", age: 35, email: "bob@example.com", password: "bob789" }
+    { name: "Manish", age: 28, email: "Manish@example.com", RollNo: "123" },
+    { name: "Bikash", age: 24, email: "Bikash@example.com", RollNo: "456" },
+    { name: "Karishma", age: 20, email: "Karishma@example.com", RollNo: "789" }
 ];
 
-function addUser() {
-    const name = document.getElementById('nameInput').value;
-    if (name.trim() === "") {
-        alert("Please fill in all fields.");
+let prvSearchInput = '';
+
+button.addEventListener('click', () => {
+    let name = nameInput.value.toLowerCase();
+
+    if (prvSearchInput === name) {
+        button.style.backgroundColor = 'green';
+        alert("Data is already available");
         return;
     }
-    users.push({
-        name: nameInput,
+
+    prvSearchInput = name;
+    let searchVal = null;
+    userDetail.innerHTML = "";
+    message.innerHTML = ""; 
+
+    users.forEach((obj) => {
+        if (obj.name.toLowerCase() === name) {
+            searchVal = obj;
+        }
     });
-    document.getElementById('name').value = '';
-}
+
+    if (searchVal !== null) {
+        const details = [
+            searchVal.name,
+            searchVal.age,
+            searchVal.email,
+            searchVal.RollNo
+        ];
+
+        let index = 0;
+
+        let id = setInterval(() => {
+            userDetail.innerHTML += `<div>${details[index]}</div>`;
+            index++;
+            if (index === details.length) {
+                clearInterval(id);
+            }
+        }, 1000);
+    } else {
+        userDetail.innerHTML = '<h2>User not found</h2>'; 
+    }
+
+    nameInput.value = "";
+});
+
+nameInput.addEventListener('keyup', () => {
+    let currentInput = nameInput.value.toLowerCase();
+    message.innerHTML = ""; 
+
+    if (prvSearchInput === currentInput) {
+        button.style.backgroundColor = 'red';
+    }else {
+        button.style.backgroundColor = 'green';
+    }
+});
