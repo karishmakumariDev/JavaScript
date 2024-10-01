226150b99eba5e8bc46f8c1204c00fee0db4153a
karishmakumariDev <kumarikarishma311329>
2024-10-01 17:15:52 +0530

    not task complet

---
+++ speed_typeing_game.js14/script.js
@@ -0,0 +1,10 @@
+const timerElement = document.getElementById('timer');
+const quoteDisplayElement = document.getElementById('quoteDisplay');
+const quoteInputElement = document.getElementById('quoteInput');
+const startBtn = document.getElementById('startBtn');
+
+let time = 60;
+
+startBtn.addEventListener('click',() => {
+
+})
